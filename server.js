6aac62a7bc433401ca877f1e56bd00750eb4d167
user <user@users-MacBook-Pro.local>
2022-04-19 15:57:03 +0100

    Sent first HTML response

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require("http");
 
-const rqListener = (req, res) => {
-  console.log(req);
-};
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  //   process.exit(); // Quits the server
 
-const server = http.createServer(rqListener);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<header><title>My first app></title></header>");
+  res.write("<body><h1>Hello from node.js app</h1></body>");
+  res.write("</html>");
+
+  res.end();
+});
 
 server.listen(8080);
